7324f995fef97d7695616b52aa62c07d9063c149
Belen <lohreley@hotmail.com>
2022-04-18 21:16:50 -0300

    unidad 3

---
+++ Unidad 3/JavaScript.js
@@ -0,0 +1,101 @@
+//1
+function noVale(){
+    let noValgoNi5 = 4;
+    return noValgoNi5;
+}
+
+//2
+function anio80(){
+    let miAnioDeNacimiento = 1991;
+    let meGustariaTener80Anios = 80;
+    let voyATener80ElAnio = miAnioDeNacimiento+meGustariaTener80Anios;
+    return voyATener80ElAnio;
+}
+
+//3
+     function dineroFaltante(){
+         let costoDeUnaTele = 10000;
+         let ahorros = 9000;
+         let dineroQueMeFalta = costoDeUnaTele - ahorros;
+         return dineroQueMeFalta;
+     }
+
+//4
+
+     function diasDecada(){
+         let diasPorAnio = 365;
+         let cantidadDeAniosEnUnaDecada = 10;
+         let cantidadDeDiasEnUnaDecada = diasPorAnio * cantidadDeAniosEnUnaDecada;
+         return cantidadDeDiasEnUnaDecada;
+     }
+
+//5
+
+     function pizza(porciones){
+         return porciones/8;
+     }
+
+//6
+
+     function animal(animal){
+         return animal == 'perro';
+     }
+
+//7
+
+     function desigualdad(){
+         return 'flan' != 'flan con dulce de leche';
+     }
+
+//8
+
+     function edad(age){
+         let soyMayorDeEdad = age > 17;
+         return soyMayorDeEdad;
+     }
+
+//9
+
+     function edad25(age){
+        let soyMenorDe25 = age < 25;
+        return soyMenorDe25;
+    }
+
+//10
+
+    function mesEnero(mes){
+        let estamosEnEnero = 'Enero' === mes;
+        return estamosEnEnero;
+    }
+
+//11
+
+    function noEnero(mes){
+        let noEstamosEnEnero = 'Enero' !== mes;
+        return noEstamosEnEnero;
+    }
+
+//12
+
+    function notaParcial(){
+        let miNotaEnElParcial = 8;
+        let notaMinimaParaAprobar = 6;
+        let estoyAprobado = miNotaEnElParcial >= notaMinimaParaAprobar;
+        return estoyAprobado;
+    }
+
+//13
+
+    function horaAlmuerzo(hora){
+        let esHoraDeAlmorzar = hora <= 12;
+        return esHoraDeAlmorzar;
+    }
+
+
+
+
+
+
+
+/*
+*/
